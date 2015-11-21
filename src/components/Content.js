6e99437567c0fae669b7f4179f811d0d586305d2
yibuyisheng <yibuyisheng@163.com>
2015-11-21 16:30:58 +0800

    ref

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,20 @@
 define(function (require) {
     var binder = require('binder');
+    var InputRow = require('./InputRow');
+    var Section = require('./Section');
 
     return binder.Component.extends(
         {
+            literalAttrReady: function () {
+                
+            },
+            componentClasses: {
+                InputRow: InputRow,
+                Section: Section
+            },
             tpl: [
                 '<div>',
-                    '<ui-input-row>',
+                    '<ui-input-row ref="row">',
                         '<ui-section width="100px">姓名：</ui-section>',
                         '<ui-section flex="1"><input type="text"></ui-section>',
                     '</ui-input-row>',
